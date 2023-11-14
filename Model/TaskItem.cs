b9967af8d618b08d98b32aa33de0c2ec009b0dd7
cloudgh <Asus ROG@DESKTOP-EG4G4F7>
2023-11-14 22:04:57 +0700

    Добавьте файлы проекта.

---
+++ Model/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TaskManager.Model
+{
+    public class TaskItem
+    {
+        public TaskItem()
+        {
+            StartDate = DateTime.Now;
+            EndDate = DateTime.Now;
+            Deadline = DateTime.Now;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public DateTime Deadline { get; set; }
+        public string AssignedTo { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.ComponentModel;
+using TaskManager.Model;
+using System;
+
+namespace TaskManager.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<TaskItem> Tasks { get; set; }
+
+        public ICommand AddTaskCommand { get; set; }
+
+        private TaskItem _newTask;
+        public TaskItem NewTask
+        {
+            get { return _newTask; }
+            set
+            {
+                _newTask = value;
+                OnPropertyChanged("NewTask");
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            Tasks = new ObservableCollection<TaskItem>();
+            AddTaskCommand = new RelayCommand(AddTask);
+            NewTask = new TaskItem();
+        }
+
+        private void AddTask(object parameter)
+        {
+            Tasks.Add(NewTask);
+            NewTask = new TaskItem();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+    }
+}
